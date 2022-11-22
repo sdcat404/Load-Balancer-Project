dd79e3b74eae710caea759c677f549827d7d88b7
Scotty <118654680+sdcat404@users.noreply.github.com>
2022-11-22 18:45:05 +0000

    Update LoadBalancer.py

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -1,6 +1,7 @@
 import customtkinter
 import requests
 import threading
+import time
 #-----------------------Pretty things----------------------#
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
@@ -14,6 +15,7 @@ root.title("Iron Load Balancer")
 def load():
     for _ in range(2):
         while True:
+                time.sleep(2)
                 request = requests.get
                 request (entry.get())
                 x = threading.Thread(target=load)
@@ -26,11 +28,19 @@ def load():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-lable = customtkinter.CTkLabel(master=frame, text="edit me", text_font=("Roboto", 24))
+lable = customtkinter.CTkLabel(master=frame, text="Iron Load Balancer", text_font=("Roboto", 24))
 lable.pack(pady=12, padx=10)
 
 entry = customtkinter.CTkEntry(master=frame, placeholder_text="Host Name / IP Address")
 entry.pack(pady=12, padx=10)
+
+checkbox1 = customtkinter.CTkCheckBox(master=frame, text="POST")
+checkbox1.pack(pady=12, padx=10)
+checkbox2 = customtkinter.CTkCheckBox(master=frame, text="GET")
+checkbox2.pack(pady=12, padx=10)
+checkbox3 = customtkinter.CTkCheckBox(master=frame, text="RANDOM")
+checkbox3.pack(pady=12, padx=10)
+
 
 button = customtkinter.CTkButton(master=frame, text="Start",text_font=("Roboto", 15), command=load)
 button.pack(pady=12, padx=10)
